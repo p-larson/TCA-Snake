d1d09c4eb9818b83df0aa7fd0fccf573b86a9757
Peter Larson <p.larson@utexas.edu>
2021-07-11 13:54:32 -0500

    Initial Commit

---
+++ Shared/SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeApp.swift
+//  Shared
+//
+//  Created by Peter Larson on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
